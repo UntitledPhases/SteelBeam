5ece7802f551e90ef7935909edaae7257f8247b0
Zak Oumar <zakoumar77@gmail.com>
2025-10-23 17:34:55 -0400

    few images, can set fav/wish/comp on cards

--- Script.js
+++ Script.js
@@ -5,18 +5,19 @@ const games = [
     { title: "Battlefield 6", img: "https://placehold.co/200x266?text=Battlefield+6" },
     { title: "Hades", img: "https://placehold.co/200x266?text=Hades" },
     { title: "Noita", img: "Images/Noita.png" },
-    { title: "Hollow Knight", img: "https://placehold.co/200x266?text=Hollow+Knight" },
+    { title: "Hollow Knight", img: "Images/hollowknight.png" },
     { title: "Terraria", img: "Images/Terraria.png" },
     { title: "Celeste", img: "https://placehold.co/200x266?text=Celeste" },
     { title: "Dark Souls 3", img: "https://placehold.co/200x266?text=Dark+Souls+3" },
     { title: "Dead Cells", img: "https://placehold.co/200x266?text=Dead+Cells" },
     { title: "Cuphead", img: "Images/Cuphead.png" },
-    { title: "Cyberpunk 2077", img: "https://placehold.co/200x266?text=Cyberpunk+2077" },
+    { title: "Cyberpunk 2077", img: "Images/cyberpunk.png" },
     { title: "God of War", img: "https://placehold.co/200x266?text=God+of+War" },
     { title: "Risk of Rain 2", img: "Images/RoR2.png" },
     { title: "Stardew Valley", img: "Images/Stardew.png" },
     { title: "Team Fortress 2", img: "Images/TF2.png" },
-    { title: "Heroes of Hammerwatch", img: "Images/HoH.png"}
+    { title: "Heroes of Hammerwatch", img: "Images/HoH.png"},
+    { title: "Tokyo Xtreme Racer", img: "Images/tokyoxtreme.png"}
 ];
 
 //sort games alphabetically by title
@@ -38,7 +39,6 @@ games.forEach((game, i) => {
     card.dataset.id = game.id //gives each card the same ID as the game it represents, we need this to actually associate cards with games
     card.innerHTML =
         `<img src="${game.img}" alt="${game.title}">`; //Fill card element with game image and alt info
-
     library.appendChild(card);  // Fill library container with cards, library is wrapper for all cards
 });
 
@@ -93,9 +93,6 @@ get.collections.favorites.push(/*<gameID>*/);
 saveData(get);
 //this updates the local copy of the localStorage data, then saves it back to user's localStorage
 
-//Ok now to add games to collections and 
-
-
 //Now to actually make the filtering buttons work
 const buttons = document.querySelectorAll("[data-filter]"); //select all buttons with data-filter attribute
 const cards = document.querySelectorAll(".card"); //select all game cards
@@ -130,5 +127,80 @@ buttons.forEach(button => {
     });
 });
 
+//reset functions on page reload, ideally this would stay
+
+//this should (hope) cycle cards through each collection when double clicked
+//border color by collection in css file
+//can't really have games in more than one though.. maybe later
+cards.forEach(card =>{
+    card.addEventListener("dblclick", (c1) =>{
+        c1.preventDefault() //don't really need but just incase
+        const gameId=Number(card.dataset.id); //get game id and data
+        const data = getData();
+
+        //variables to check where game is rn
+        const inFavorites = data.collections.favorites.includes(gameId);
+        const inWishlist = data.collections.wishlist.includes(gameId);
+        const inCompleted = data.collections.completed.includes(gameId);
+
+        //self explanatory me thinks
+        let currentstate = "none";
+        if (inFavorites) currentstate = "favorites";
+        else if (inWishlist) currentstate = "wishlist";
+        else if (inCompleted) currentstate = "completed";
+
+        switch (currentstate) {
+            case "none":
+                //not in any collection, add to favorites
+                data.collections.favorites.push(gameId);
+                card.classList.add("in-favorites");
+                break;
+            case "favorites":
+                //in favorites, move to wishlist
+                data.collections.favorites = data.collections.favorites.filter(id => id !== gameId);
+                data.collections.wishlist.push(gameId);
+                card.classList.remove("in-favorites");
+                card.classList.add("in-wishlist");
+                break;
+            case "wishlist":
+                //in wishlist, move to completed
+                data.collections.wishlist = data.collections.wishlist.filter(id => id !== gameId);
+                data.collections.completed.push(gameId);
+                card.classList.remove("in-wishlist");
+                card.classList.add("in-completed");
+                break;
+            case "completed":
+                //in completed, remove from all collections
+                data.collections.completed = data.collections.completed.filter(id => id !== gameId);
+                card.classList.remove("in-completed");
+                break;
+        }
+        saveData(data);
+        console.log("updated collections", data.collections);
+
+    });
+});
+//works mostly ok but sometimes you need to hover off when changing multiple times, not important
+
+function restoreCardBorder(){ //cards weren't keeping color on page refresh so this exists
+    const data = getData();
+    cards.forEach(card=>{
+        const gameId=Number(card.dataset.id);
+        card.classList.remove("in-favorites", "in-wishlist", "in-completed"); //remove first
+        //check collection to see if card is in it then add class back
+        if (data.collections.favorites.includes(gameId)){
+            card.classList.add("in-favorites")
+        }
+        else if (data.collections.wishlist.includes(gameId)){
+            card.classList.add("in-wishlist")
+        }
+        else if (data.collections.completed.includes(gameId)){
+            card.classList.add("in-completed")
+        }
+    })
+}
+
 //initially show all cards
 filterCards("all");
+//keep card border colors
+restoreCardBorder();
